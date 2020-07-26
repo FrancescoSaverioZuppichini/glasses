df7f9ed3c85095a12c8761c74b1a65d96aa85432
Francesco Saverio Zuppichini <francesco.zuppichini@gmail.com>
2020-07-26 11:10:27 +0200

    Feature/core #4 (#30)
    * minor changes
    
    * naming
    
    BottleNeck -> BottleNeck
    
    * fixed senet

--- eyes/nn/senet/__init__.py
+++ eyes/nn/senet/__init__.py
@@ -1,4 +1,4 @@
-from ..resnet import ResNetBasicBlock, ResNetBottleNeckBlock, ResNet
+from ..resnet import ResNetBasicBlock, ResNetBottleneckBlock, ResNet
 from ..se import SEModule
 
 class SENetBasicBlock(ResNetBasicBlock):
@@ -8,7 +8,7 @@ class SENetBasicBlock(ResNetBasicBlock):
         
 
 
-class SENetBottleNeckBlock(ResNetBottleNeckBlock):
+class SENetBottleneckBlock(ResNetBottleNeckBlock):
     def __init__(self, in_features: int, out_features: int, reduction: int =16, *args, **kwargs):
         super().__init__(in_features, out_features, *args, **kwargs)
         self.block.add_module('se', SEModule(out_features))
@@ -23,12 +23,12 @@ def se_resnet34(*args, **kwargs) -> ResNet:
 
 
 def se_resnet50(*args, **kwargs) -> ResNet:
-    return ResNet(*args, **kwargs, block=SENetBottleNeckBlock, deepths=[3, 4, 6, 3])
+    return ResNet(*args, **kwargs, block=SENetBottleneckBlock, deepths=[3, 4, 6, 3])
 
 
 def se_resnet101(*args, **kwargs) -> ResNet:
-    return ResNet(*args, **kwargs, block=SENetBottleNeckBlock, deepths=[3, 4, 23, 3])
+    return ResNet(*args, **kwargs, block=SENetBottleneckBlock, deepths=[3, 4, 23, 3])
 
 
 def se_resnet152(*args, **kwargs) -> ResNet:
-    return ResNet(*args, **kwargs, block=SENetBottleNeckBlock, deepths=[3, 8, 36, 3])
+    return ResNet(*args, **kwargs, block=SENetBottleneckBlock, deepths=[3, 8, 36, 3])
