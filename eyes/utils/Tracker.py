97b0e2d1c3b463b7f100d57dff063a06690af818
Francesco Saverio Zuppichini <francesco.zuppichini@gmail.com>
2020-07-26 09:12:27 +0200

    tested module transfer (#25)

---
+++ eyes/utils/Tracker.py
@@ -0,0 +1,54 @@
+import torch
+import copy
+import torch.nn as nn
+from torch import Tensor
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Tracker:
+    """This class tracks all the operations of a given module by performing a forward pass. 
+    
+    :Example
+
+        >>> import torch
+        >>> import torch.nn as nn
+        >>> from eyes.utils import Tracker
+        >>> model = nn.Sequential(nn.Linear(1, 64), nn.ReLU(), nn.Linear(64,10), nn.ReLU())
+        >>> tr = Tracker(model)
+        >>> tr(x)
+        >>> print(tr.traced) # all operations
+        >>> print('-----')
+        >>> print(tr.parametrized) # all operations with learnable params
+        
+        outputs 
+        
+        [Linear(in_features=1, out_features=64, bias=True),
+        ReLU(),
+        Linear(in_features=64, out_features=10, bias=True),
+        ReLU()]
+        -----
+        [Linear(in_features=1, out_features=64, bias=True),
+        Linear(in_features=64, out_features=10, bias=True)]
+    """
+    module: nn.Module
+    traced: [nn.Module] = field(default_factory=list)
+    handles: list = field(default_factory=list)
+
+    def _forward_hook(self, m, inputs: Tensor, outputs: Tensor):
+        has_not_submodules = len(list(m.modules())) == 1
+        if has_not_submodules:
+            self.traced.append(m)
+
+    def __call__(self, x: Tensor) -> Tensor:
+        for m in self.module.modules():
+            self.handles.append(
+                m.register_forward_hook(self._forward_hook))
+        self.module(x)
+        list(map(lambda x: x.remove(), self.handles))
+        return self
+
+    @property
+    def parametrized(self):
+        # check the len of the state_dict keys to see if we have learnable params
+        return list(filter(lambda x: len(list(x.state_dict().keys())) > 0, self.traced))
---
+++ test/test_ModuleTransfer.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+from eyes.utils.ModuleTransfer import ModuleTransfer
+from eyes.utils.Tracker import Tracker
+
+def test_ModuleTransfer():
+    model_a = nn.Sequential(nn.Linear(1, 64), nn.ReLU(), nn.Linear(64,10), nn.ReLU())
+    def block(in_features, out_features):
+        return nn.Sequential(nn.Linear(in_features, out_features),
+                        nn.ReLU())
+    model_b = nn.Sequential(block(1,64), block(64,10))
+    # model_a and model_b are the same thing but defined in two different ways
+    x = torch.ones(1, 1)
+    trans = ModuleTransfer(src=model_a, dest=model_b, verbose=1)
+    trans(x)
+    
+    src_traced = Tracker(model_a)(x).parametrized
+    dest_traced = Tracker(model_b)(x).parametrized
+
+    for dest_m, src_m in zip(dest_traced, src_traced):
+        for key in src_m.state_dict().keys():
+            for a, b in zip(src_m.state_dict()[key], dest_m.state_dict()[key]):
+                assert torch.equal(a, b)
