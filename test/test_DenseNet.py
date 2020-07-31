cf7085f1985f31b3c080c066a7aa95cfc46800d4
Francesco <zuppif@usi.ch>
2020-07-31 17:46:33 +0200

    added tests

--- test/test_DenseNet.py
+++ test/test_DenseNet.py
@@ -2,7 +2,7 @@ import torch
 from glasses.nn.models.classification.densenet import DenseNet
 
 
-def test_alexnet():
+def test_DenseNet():
     x = torch.rand(1, 3,224,224)
     model = DenseNet().eval()
     pred = model(x)
