06ba69d18309265b1328e16cbf46b0a71770c727
Francesco <zuppif@usi.ch>
2020-08-18 15:03:59 +0200

    old template again

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-# import sphinx_rtd_theme
+import sphinx_rtd_theme
 import pytorch_sphinx_theme
 import os
 import sys
@@ -57,8 +57,14 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "pytorch_sphinx_theme"
-
+html_theme = "sphinx_rtd_theme"
+# html_theme_options = {
+#     'pytorch_project': 'docs',
+#     'canonical_url': 'https://pytorch.org/docs/stable/',
+#     'collapse_navigation': False,
+#     'display_version': True,
+#     'logo_only': True,
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
