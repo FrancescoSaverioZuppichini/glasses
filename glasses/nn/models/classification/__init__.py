f79e76d4cabee150d67441ef16ebd3f168fe6045
Francesco <zuppif@usi.ch>
2020-09-08 21:07:42 +0200

    done

--- glasses/nn/models/classification/__init__.py
+++ glasses/nn/models/classification/__init__.py
@@ -4,8 +4,8 @@ from .resnet import ResNet
 from .densenet import DenseNet
 from .senet import SEResNet
 from .vgg import VGG
-from .se import SEModule
+from .se import SSEModule, CSEModule, SCSEModule
 from .mobilenet import MobileNetV2
 from .alexnet import AlexNet
 
-__all__ = ['ResNet', 'DenseNet','SEModule', 'SEResNet', 'VGG', 'MobileNetV2', 'AlexNet', ]
+__all__ = ['ResNet', 'DenseNet', 'SSEModule', 'CSEModule', 'SCSEModule', 'SEResNet', 'VGG', 'MobileNetV2', 'AlexNet', ]
--- test/test_SEModule.py
+++ test/test_SEModule.py
@@ -1,11 +1,27 @@
 import torch
 import torch.nn as nn
-from glasses.nn.models.classification.se import SEModule
+from glasses.nn.models.classification.se import SCSEModule, CSEModule, SSEModule
 
 def test_tracker():
     x = torch.rand(1,48,8,8)
-    se = SEModule(x.shape[1])
+
+    se = SSEModule(x.shape[1])
     res = se(x)
 
     assert res.shape == x.shape
+
+    se = SSEModule(x.shape[1], reduced_features=10)
+
+    assert se.att[0].out_features == 10
+
+    se = CSEModule(x.shape[1])
+    res = se(x)
+
+    assert res.shape == x.shape
+
+    se = SCSEModule(x.shape[1])
+    res = se(x)
+
+    assert res.shape == x.shape
+ 
     
