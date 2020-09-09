12baea20889a6d97e3bc073dee7b9a4da968b6e9
zuppif <francesco.zuppichini@gmail.com>
2020-09-09 20:41:51 +0200

    fixed test

--- test/test_SEModule.py
+++ test/test_SEModule.py
@@ -10,7 +10,7 @@ def test_tracker():
 
     assert res.shape == x.shape
 
-    se = SSEModule(x.shape[1], reduced_features=10)
+    se = SpatialChannelSE(x.shape[1], reduced_features=10)
 
     assert se.att.fc1.out_features == 10
 
