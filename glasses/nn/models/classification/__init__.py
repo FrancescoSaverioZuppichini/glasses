eb5137f618e56f7d5e2f32269dcf34256972f3f5
Francesco <zuppif@usi.ch>
2020-09-13 16:55:57 +0200

    minor changes

--- glasses/nn/models/classification/__init__.py
+++ glasses/nn/models/classification/__init__.py
@@ -7,5 +7,6 @@ from .vgg import VGG
 from .se import SpatialSE, ChannelSE, SpatialChannelSE
 from .mobilenet import MobileNetV2
 from .alexnet import AlexNet
+from .efficientnet import EfficientNet
 
-__all__ = ['ResNet', 'DenseNet', 'SpatialSE', 'ChannelSE', 'SpatialChannelSE', 'SEResNet', 'VGG', 'MobileNetV2', 'AlexNet', ]
+__all__ = ['ResNet', 'DenseNet', 'SpatialSE', 'ChannelSE', 'SpatialChannelSE', 'SEResNet', 'VGG', 'MobileNetV2', 'AlexNet',  'EfficientNet']
