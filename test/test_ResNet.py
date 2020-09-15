61926732b43395bb7b57b7a02486b1ec001a84d9
Francesco <zuppif@usi.ch>
2020-09-15 16:11:02 +0200

    fixed tests

--- test/test_ResNet.py
+++ test/test_ResNet.py
@@ -1,7 +1,7 @@
 import torch
 from glasses.nn.models.classification.resnet import ResNet, ResNetBasicPreActBlock, ResNetBottleneckPreActBlock
 from glasses.nn.models.classification.resnetxt import ResNetXt
-from glasses.nn.models.classification.wide_resnet import WideResNet
+from glasses.nn.models.classification.wide_resnet import WideResNet, WideResnetBottleNeckBlock
 
 def test_resnet():
     x = torch.rand(1, 3, 224, 224)
@@ -44,7 +44,7 @@ def test_resnetxt():
     pred = model(x)
     assert pred.shape[-1] == 1000
 
-def rest_wide_resnet():
+def test_wide_resnet():
     x = torch.rand(1, 3, 224, 224)
     model = WideResNet.wide_resnet50_2().eval()
     pred = model(x)
@@ -53,3 +53,10 @@ def rest_wide_resnet():
     model = WideResNet.wide_resnet101_2().eval()
     pred = model(x)
     assert pred.shape[-1] == 1000
+
+
+    block = WideResnetBottleNeckBlock(32, 32, width_factor=3)
+
+    print(block)
+
+    assert block.block.block.conv2.in_channels ==  32  * 3
