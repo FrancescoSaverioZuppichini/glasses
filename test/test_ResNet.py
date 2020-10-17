4f500fbfbfec6048c2e28fd4752730763b54df8b
Francesco <zuppif@usi.ch>
2020-10-17 14:10:37 +0200

    minor changes

--- test/test_ResNet.py
+++ test/test_ResNet.py
@@ -6,6 +6,7 @@ from glasses.nn.models.classification.wide_resnet import WideResNet, WideResnetB
 def test_resnet():
     x = torch.rand(1, 3, 224, 224)
     model = ResNet.resnet18().eval()
+    model.summary()
     pred = model(x)
     assert pred.shape[-1] == 1000
 
