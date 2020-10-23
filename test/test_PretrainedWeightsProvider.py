57893919b73d0b6244fcb92b3f7057eca32bb024
Francesco <zuppif@usi.ch>
2020-10-23 14:35:40 +0200

    minor changes

--- test/test_PretrainedWeightsProvider.py
+++ test/test_PretrainedWeightsProvider.py
@@ -1,19 +1,21 @@
 import torch
 import torch.nn as nn
-from glasses.utils.PretrainedWeightsProvider import PretrainedWeightsProvider, Config, pretrained
+from glasses.utils.PretrainedWeightsProvider import PretrainedWeightsProvider
 from glasses.nn.models.classification.resnet import ResNet
 import pytest
-from torchvision.transforms import Compose
 from pathlib import Path
 import os
 
+
 def test_PretrainedWeightsProvider():
     provider = PretrainedWeightsProvider()
+    provider.BASE_DIR = './'
 
-    config = Config()
-    tr = config.transform
-
-    assert type(tr) is Compose
+    provider.BASE_URL = 'https://github.com/FrancescoSaverioZuppichini/glasses/blob/develop/test/resnet18.pth?raw=true'
+    provider.download_weight('https://github.com/FrancescoSaverioZuppichini/glasses/blob/develop/test/resnet18.pth?raw=true',
+                             save_path=provider.BASE_DIR / Path('resnet18.pth'))
+    # with pytest.raises(KeyError):
+    #     provider['does_not_exist']
 
     resnet18_state = ResNet.resnet18().state_dict()
     resnet18_prov_state = provider['resnet18']
@@ -21,28 +23,12 @@ def test_PretrainedWeightsProvider():
     for mod, mod_prov in zip(resnet18_state.keys(), resnet18_prov_state.keys()):
         assert str(mod) == str(mod_prov)
 
+    # this test will take too much time!
+    # for key, models in PretrainedWeightsProvider.zoo_models_mapping.items():
+    # _, model_def = models
+    # model = model_def()
+    # model_prov = provider[key]
 
+    # for mod, mod_prov in zip(model.children(), model_prov.children()):
+    #     assert str(mod) ==  str(mod_prov)
 
-# need to find another place to host the weights!
-# def test_PretrainedWeightsProvider_download_weight():
-#     provider = PretrainedWeightsProvider()
-
-#     save_path = Path('/tmp/resnet18.pt')
-#     assert not save_path.exists()
-    
-#     provider = PretrainedWeightsProvider()
-#     provider.download_weight(PretrainedWeightsProvider.BASE_URL + '/resnet18.pt', save_path=save_path)
-#     assert save_path.exists()
-
-#     os.remove(save_path)
-
-def test_pretrained():
-    # not really a great test!
-    x = torch.rand(1, 3,224,224)
-    model_pretrained = ResNet.resnet18(pretrained=True)
-    pred = model_pretrained(x)
-    assert pred.shape[-1] == 1000
-
-
-    
-    
