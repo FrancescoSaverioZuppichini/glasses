280175a25eabd512de2d9897781dc6cee70d5c27
FrancescoSaverioZuppichini <zuppif@usi.ch>
2020-10-24 09:34:57 +0000

    Deploying to gh-pages from  @ 309a86a1f8e1357f4d5f8672df5792e38c3c5e6a 🚀

--- conf.py
+++ conf.py
@@ -52,6 +52,9 @@ autosummary_generate = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+autodoc_default_options = {
+    'undoc-members': 'forward',
+}
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
