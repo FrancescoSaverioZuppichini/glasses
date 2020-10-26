712b3ee9a7b9aede8df47e5fbd59e3d08017fe16
Francesco <zuppif@usi.ch>
2020-10-26 17:08:15 +0100

    added deploy runner

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("./README.md", "r") as fh:
 
 setup(
     name="glasses", 
-    version="0.0.1",
+    version="0.0.2",
     author="Francesco Saverio Zuppichini & Francesco Cicala",
     author_email="francesco.zuppichini@gmail.com",
     description="Compact, concise and customizable deep learning computer vision",
