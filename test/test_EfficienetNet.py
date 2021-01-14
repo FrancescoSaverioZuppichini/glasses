e5dfc0e0fd1aa52689a16b6ab3c2ca93d302b2bf
francesco <francesco.zuppichini@gmail.com>
2021-01-14 17:42:18 +0100

    tests

--- test/test_EfficienetNet.py
+++ test/test_EfficienetNet.py
@@ -5,20 +5,20 @@ from glasses.models.classification import EfficientNet, EfficientNetLite
 
 def test_EfficientNet():
     x = torch.rand((1, 3, 224, 224))
-    model = EfficientNet.efficientnet_b0()
+    model = EfficientNet.efficientnet_b0().eval()
     pred = model(x)
     assert pred.shape[-1] == 1000
     
 
 def test_EfficientNetb1():
     x = torch.rand(1, 3, 224, 224)
-    model = EfficientNet.efficientnet_b1()
+    model = EfficientNet.efficientnet_b1().eval()
     pred = model(x)
     assert pred.shape[-1] == 1000
 
 def test_EfficientNetb2():
     x = torch.rand(1, 3, 224, 224)
-    model = EfficientNet.efficientnet_b2()
+    model = EfficientNet.efficientnet_b2().eval()
     model.encoder.features
     pred = model(x)
     # test also features
@@ -31,38 +31,38 @@ def test_EfficientNetb2():
 
 def test_EfficientNetb3():
     x = torch.rand(1, 3, 224, 224)
-    model = EfficientNet.efficientnet_b3()
+    model = EfficientNet.efficientnet_b3().eval()
     pred = model(x)
     assert pred.shape[-1] == 1000
 
 def test_EfficientNetb4():
     x = torch.rand(1, 3, 224, 224)
-    model = EfficientNet.efficientnet_b4()
+    model = EfficientNet.efficientnet_b4().eval()
     pred = model(x)
     assert pred.shape[-1] == 1000
 
 def test_EfficientNetb5():
     x = torch.rand(1, 3, 224, 224)
-    model = EfficientNet.efficientnet_b5()
+    model = EfficientNet.efficientnet_b5().eval()
     pred = model(x)
     assert pred.shape[-1] == 1000
 
 def test_EfficientNetb6():
     x = torch.rand(1, 3, 224, 224)
-    model = EfficientNet.efficientnet_b6()
+    model = EfficientNet.efficientnet_b6().eval()
     pred = model(x)
     assert pred.shape[-1] == 1000
 
 def test_EfficientNetb7():
     x = torch.rand(1, 3, 224, 224)
-    model = EfficientNet.efficientnet_b7()
+    model = EfficientNet.efficientnet_b7().eval()
     pred = model(x)
     assert pred.shape[-1] == 1000
 
 
 def test_EfficientNetb8():
     x = torch.rand(1, 3, 224, 224)
-    model = EfficientNet.efficientnet_b8()
+    model = EfficientNet.efficientnet_b8().eval()
     pred = model(x)
     assert pred.shape[-1] == 1000
 
