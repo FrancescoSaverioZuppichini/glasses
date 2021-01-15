28ef04801ca2aa51db8dbb019df8ab3e4aa5ed3a
francesco <francesco.zuppichini@gmail.com>
2021-01-15 09:28:04 +0100

    dropblock

--- test/test_ResNeSt.py
+++ test/test_ResNeSt.py
@@ -7,29 +7,29 @@ from glasses.models.classification.resnest import *
 def test_resnest():
     x = torch.rand(1, 3, 224, 224)
     with torch.no_grad():
-        model = ResNetSt.resnest14d().eval()
+        model = ResNeSt.resnest14d().eval()
         pred = model(x)
         assert pred.shape[-1] == 1000
         n_classes = 10
-        model = ResNetSt.resnest14d(n_classes=n_classes).eval()
+        model = ResNeSt.resnest14d(n_classes=n_classes).eval()
         pred = model(x)
         assert pred.shape[-1] == n_classes
-        model = ResNetSt.resnest26d().eval()
+        model = ResNeSt.resnest26d().eval()
         pred = model(x)
         assert pred.shape[-1] == 1000
-        model = ResNetSt.resnest50d().eval()
+        model = ResNeSt.resnest50d().eval()
         pred = model(x)
         assert pred.shape[-1] == 1000
-        model = ResNetSt.resnest50d_1s4x24d().eval()
+        model = ResNeSt.resnest50d_1s4x24d().eval()
         pred = model(x)
         assert pred.shape[-1] == 1000
-        model = ResNetSt.resnest50d_4s2x40d().eval()
+        model = ResNeSt.resnest50d_4s2x40d().eval()
         pred = model(x)
         assert pred.shape[-1] == 1000
-        model = ResNetSt.resnest101e().eval()
+        model = ResNeSt.resnest101e().eval()
         pred = model(x)
         assert pred.shape[-1] == 1000
-        model = ResNetSt.resnest50d_fast().eval()
+        model = ResNeSt.resnest50d_fast().eval()
         pred = model(x)
         assert pred.shape[-1] == 1000
         # too big!
