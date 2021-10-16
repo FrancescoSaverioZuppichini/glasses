a632fe2d1d87273fc9fbe463bba4cd9eff1d7dec
fra <francesco.zuppichini@gmail.com>
2021-10-16 12:39:44 +0200

    tests for se models

--- test/test_SENet.py
+++ test/test_SENet.py
@@ -3,43 +3,24 @@ from glasses.models.classification import SEResNet
 
 
 def test_seresnet():
-    x = torch.rand(1, 3, 224, 224)
-    model = SEResNet.se_resnet18().eval()
-    pred = model(x)
-    assert pred.shape[-1] == 1000
+    with torch.no_grad():
+        x = torch.rand(1, 3, 224, 224)
+        model = SEResNet.se_resnet18().eval()
+        pred = model(x)
+        assert pred.shape[-1] == 1000
 
-    model = SEResNet.se_resnet34().eval()
-    pred = model(x)
-    assert pred.shape[-1] == 1000
+        model = SEResNet.se_resnet34().eval()
+        pred = model(x)
+        assert pred.shape[-1] == 1000
 
-    model = SEResNet.se_resnet50().eval()
-    pred = model(x)
-    assert pred.shape[-1] == 1000
+        model = SEResNet.se_resnet50().eval()
+        pred = model(x)
+        assert pred.shape[-1] == 1000
 
-    model = SEResNet.se_resnet101().eval()
-    pred = model(x)
-    assert pred.shape[-1] == 1000
+        model = SEResNet.se_resnet101().eval()
+        pred = model(x)
+        assert pred.shape[-1] == 1000
 
-    model = SEResNet.se_resnet152().eval()
-    pred = model(x)
-    assert pred.shape[-1] == 1000
-
-    model = SEResNet.cse_resnet18().eval()
-    pred = model(x)
-    assert pred.shape[-1] == 1000
-
-    model = SEResNet.cse_resnet34().eval()
-    pred = model(x)
-    assert pred.shape[-1] == 1000
-
-    model = SEResNet.cse_resnet50().eval()
-    pred = model(x)
-    assert pred.shape[-1] == 1000
-
-    model = SEResNet.cse_resnet101().eval()
-    pred = model(x)
-    assert pred.shape[-1] == 1000
-
-    model = SEResNet.cse_resnet152().eval()
-    pred = model(x)
-    assert pred.shape[-1] == 1000
+        model = SEResNet.se_resnet152().eval()
+        pred = model(x)
+        assert pred.shape[-1] == 1000
